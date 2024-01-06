ff835935ef81f674acee729306fb35ac33b039f8
yonghun <asqwklop12@gmail.com>
2024-01-06 23:16:40 +0900

    validation 체크

--- src/main/java/com/findting/dto/board/exception/ErrorCode.java
+++ src/main/java/com/findting/dto/board/exception/ErrorCode.java
@@ -7,12 +7,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Getter
-@RequiredArgsConstructor
 public class ErrorCode {
     private final String code;
     private final String message;
 
-    private Map<String, String> validation = new HashMap<>();
+    public ErrorCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    // validation 체크
+    private final Map<String, String> validation = new HashMap<>();
 
     public void addValidation(String filed, String validation) {
         this.validation.put(filed, validation);
