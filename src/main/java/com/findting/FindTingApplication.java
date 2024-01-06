de406baba7ae6f226def6350f1aceabdf915c968
yonghun <asqwklop12@gmail.com>
2024-01-06 15:21:07 +0900

    프로젝트 start

---
+++ src/main/java/com/findting/FindTingApplication.java
@@ -0,0 +1,13 @@
+package com.findting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindTingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindTingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/findting/FindTingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindTingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
