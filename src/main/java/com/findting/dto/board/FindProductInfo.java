0b5164db2db5293eff197bad211ae60bdc3c0005
yonghun <asqwklop12@gmail.com>
2024-01-13 16:31:36 +0900

    final 추가

--- src/main/java/com/findting/dto/board/FindProductInfo.java
+++ src/main/java/com/findting/dto/board/FindProductInfo.java
@@ -10,12 +10,12 @@ public class FindProductInfo {
     @NotBlank(message = "종류를 입력해주세요.")
     @NotEmpty(message = "종류를 입력해주세요.")
     @NotNull(message = "종류를 입력해주세요.")
-    private String kind;
+    private final String kind;
 
     @NotBlank(message = "특징을 설명해주세요.")
     @NotEmpty(message = "특징을 설명해주세요.")
     @NotNull(message = "특징을 설명해주세요.")
-    private String detail;
+    private final String detail;
 
     public FindProductInfo(@NotNull(message = "종류를 입력해주세요.") String kind, @NotNull(message = "특징을 설명해주세요.") String detail) {
         this.kind = kind;
